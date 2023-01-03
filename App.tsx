9cd20e595e822ddec7ec8c4b4900a2599b2a760f
Jere Penttinen <jere@disroot.org>
2023-01-03 13:22:06 +0200

    Eslint, prettier, RNUILib setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { View, Text } from "react-native-ui-lib";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View flex paddingH-25 paddingT-120>
+      <Text blue50 text20>
+        Moi
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
