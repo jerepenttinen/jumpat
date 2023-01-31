2fe2b800f36b65531bd66bc9b7e1155df1d744e7
Jere Penttinen <jere@disroot.org>
2023-01-31 12:35:16 +0200

    Make theme adapt to device dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,21 @@
 import "~/presentation/locales";
 import "~/data";
-import {
-  NavigationContainer,
-  DefaultTheme as NavigationLightTheme,
-} from "@react-navigation/native";
-import {
-  MD3LightTheme,
-  Provider as PaperProvider,
-  adaptNavigationTheme,
-} from "react-native-paper";
+import { NavigationContainer } from "@react-navigation/native";
+import { Provider as PaperProvider } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import usePaperTheme from "./presentation/hooks/usePaperTheme";
 
 import { AppLocaleProvider } from "~/presentation/locales/locale";
 import Navigator from "~/presentation/navigation/Navigator";
 
 export default function App() {
-  const { LightTheme } = adaptNavigationTheme({
-    reactNavigationLight: NavigationLightTheme,
-  });
+  const [paperTheme, navigationTheme] = usePaperTheme();
+
   return (
     <SafeAreaProvider>
-      <PaperProvider theme={MD3LightTheme}>
-        <NavigationContainer theme={LightTheme}>
+      <PaperProvider theme={paperTheme}>
+        <NavigationContainer theme={navigationTheme}>
           <AppLocaleProvider>
             <Navigator />
           </AppLocaleProvider>
