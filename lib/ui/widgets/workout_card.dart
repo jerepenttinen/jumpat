34b3b7eccfccd17ae7c1750a6ef835151004e35a
Jere Penttinen <jere@disroot.org>
2023-02-18 11:59:08 +0200

    Edit and delete workout

--- lib/ui/widgets/workout_card.dart
+++ lib/ui/widgets/workout_card.dart
@@ -1,5 +1,11 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:jumpat/data/isar_service.dart';
 import 'package:jumpat/data/workout.dart';
+import 'package:jumpat/injection.dart';
+import 'package:jumpat/ui/routes/app_router.dart';
+
+enum CardMenuItem { edit, delete }
 
 class WorkoutCard extends StatelessWidget {
   const WorkoutCard({required this.workout, super.key});
@@ -15,7 +21,29 @@ class WorkoutCard extends StatelessWidget {
         children: [
           ListTile(
             title: Text(workout.date.toIso8601String()),
-            trailing: const Icon(Icons.more_vert),
+            trailing: PopupMenuButton<CardMenuItem>(
+              icon: const Icon(Icons.more_vert),
+              onSelected: (value) {
+                switch (value) {
+                  case CardMenuItem.edit:
+                    context.router.push(EditWorkoutRoute(workout: workout));
+                    break;
+                  case CardMenuItem.delete:
+                    getIt<IsarService>().deleteWorkout(workout);
+                    break;
+                }
+              },
+              itemBuilder: (context) => [
+                const PopupMenuItem(
+                  value: CardMenuItem.edit,
+                  child: Text('Muokkaa'),
+                ),
+                const PopupMenuItem(
+                  value: CardMenuItem.delete,
+                  child: Text('Poista'),
+                ),
+              ],
+            ),
           ),
           Container(
             padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
