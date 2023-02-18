3e9118724850de0165894324a1604b400bcda171
Jere Penttinen <jere@disroot.org>
2023-02-18 12:23:20 +0200

    Edit workout date

--- lib/ui/edit_workout_page.dart
+++ lib/ui/edit_workout_page.dart
@@ -3,15 +3,40 @@ import 'package:jumpat/data/isar_service.dart';
 import 'package:jumpat/data/workout.dart';
 import 'package:jumpat/injection.dart';
 
-class EditWorkoutPage extends StatelessWidget {
+class EditWorkoutPage extends StatefulWidget {
   const EditWorkoutPage({required this.workout, super.key});
   final Workout workout;
 
   @override
+  State<EditWorkoutPage> createState() => _EditWorkoutPageState();
+}
+
+class _EditWorkoutPageState extends State<EditWorkoutPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(workout.date.toIso8601String()),
+        title: Text(widget.workout.date.toIso8601String()),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final newDate = await showDatePicker(
+                context: context,
+                initialDate: widget.workout.date,
+                firstDate: DateTime(2010),
+                lastDate: DateTime(2050),
+              );
+
+              if (newDate != null) {
+                setState(() {
+                  widget.workout.date = newDate;
+                });
+                getIt<IsarService>().saveWorkout(widget.workout);
+              }
+            },
+            icon: const Icon(Icons.calendar_today),
+          )
+        ],
       ),
       body: _buildMovementsList(context),
     );
@@ -19,7 +44,7 @@ class EditWorkoutPage extends StatelessWidget {
 
   StreamBuilder<List<Movement>> _buildMovementsList(BuildContext context) {
     return StreamBuilder<List<Movement>>(
-      stream: getIt<IsarService>().watchMovements(workout),
+      stream: getIt<IsarService>().watchMovements(widget.workout),
       builder: (context, snapshot) {
         final movements = snapshot.data ?? [];
         return ListView.builder(
