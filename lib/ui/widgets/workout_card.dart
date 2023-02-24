a4d093434c9e7febc726c5086be0165c6774e7cc
Jere Penttinen <jere@disroot.org>
2023-02-24 21:51:06 +0200

    Switch to border color

--- lib/ui/widgets/workout_card.dart
+++ lib/ui/widgets/workout_card.dart
@@ -20,102 +20,78 @@ class WorkoutCard extends ConsumerWidget {
     final movementsAsync = ref.watch(watchMovementsProvider(workout));
     final templateAsync = ref.watch(watchWorkoutTemplateProvider(workout));
     final t = AppLocalizations.of(context)!;
-    return Theme(
-      data: Theme.of(context).copyWith(
-        textTheme: templateAsync.maybeWhen(
-          data: (template) => template == null
-              ? null
-              : Color(template.color).computeLuminance() > 0.5
-                  ? Typography().black
-                  : Typography().white,
-          orElse: () => null,
-        ),
-        listTileTheme: templateAsync.maybeWhen(
-          data: (template) => template == null
-              ? null
-              : ListTileThemeData(
-                  iconColor: Color(template.color).computeLuminance() > 0.5
-                      ? Typography().black.bodySmall!.color
-                      : Typography().white.bodyMedium!.color,
-                ),
-          orElse: () => null,
-        ),
-        popupMenuTheme: templateAsync.maybeWhen(
-          data: (template) => template == null
-              ? null
-              : PopupMenuThemeData(color: Color(template.color)),
-          orElse: () => null,
-        ),
+    return Card(
+      elevation: 1,
+      shape: templateAsync.maybeWhen(
+        data: (template) => template == null
+            ? null
+            : RoundedRectangleBorder(
+                side: BorderSide(color: Color(template.color), width: 2),
+                borderRadius: BorderRadius.circular(4),
+              ),
+        orElse: () => null,
       ),
-      child: Card(
-        elevation: 1,
-        color: templateAsync.maybeWhen(
-          data: (template) => template == null ? null : Color(template.color),
-          orElse: () => null,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ListTile(
-              title: Text(t.workoutDate(workout.date)),
-              subtitle: templateAsync.maybeWhen(
-                data: (template) =>
-                    template == null ? null : Text(template.name),
-                orElse: () => null,
-              ),
-              trailing: PopupMenuButton<CardMenuItem>(
-                icon: const Icon(Icons.more_vert),
-                onSelected: (value) async {
-                  switch (value) {
-                    case CardMenuItem.edit:
-                      context.router.push(EditWorkoutRoute(workout: workout));
-                      break;
-                    case CardMenuItem.delete:
-                      confirmDelete(context).then((value) {
-                        if (value) {
-                          ref.read(deleteWorkoutProvider(workout));
-                        }
-                      });
-                      break;
-                    case CardMenuItem.asTemplate:
-                      await showCreateTemplateDialog(context, workout);
-                      break;
-                  }
-                },
-                itemBuilder: (context) => [
-                  PopupMenuItem(
-                    value: CardMenuItem.edit,
-                    child: Text(t.edit),
-                  ),
-                  PopupMenuItem(
-                    value: CardMenuItem.asTemplate,
-                    child: Text(t.asTemplate),
-                  ),
-                  PopupMenuItem(
-                    value: CardMenuItem.delete,
-                    child: Text(t.delete),
-                  ),
-                ],
-              ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ListTile(
+            title: Text(t.workoutDate(workout.date)),
+            subtitle: templateAsync.maybeWhen(
+              data: (template) => template == null ? null : Text(template.name),
+              orElse: () => null,
             ),
-            Container(
-              padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
-              child: movementsAsync.maybeWhen(
-                data: (movements) {
-                  return ListBody(
-                    children: movements
-                        .map(
-                          (movement) =>
-                              WorkoutCardMovementLine(movement: movement),
-                        )
-                        .toList(),
-                  );
-                },
-                orElse: () => const SizedBox(),
-              ),
+            trailing: PopupMenuButton<CardMenuItem>(
+              icon: const Icon(Icons.more_vert),
+              onSelected: (value) async {
+                switch (value) {
+                  case CardMenuItem.edit:
+                    context.router.push(EditWorkoutRoute(workout: workout));
+                    break;
+                  case CardMenuItem.delete:
+                    confirmDelete(context).then((value) {
+                      if (value) {
+                        ref.read(deleteWorkoutProvider(workout));
+                      }
+                    });
+                    break;
+                  case CardMenuItem.asTemplate:
+                    await showCreateTemplateDialog(context, workout);
+                    break;
+                }
+              },
+              itemBuilder: (context) => [
+                PopupMenuItem(
+                  value: CardMenuItem.edit,
+                  child: Text(t.edit),
+                ),
+                PopupMenuItem(
+                  value: CardMenuItem.asTemplate,
+                  child: Text(t.asTemplate),
+                ),
+                PopupMenuItem(
+                  value: CardMenuItem.delete,
+                  child: Text(t.delete),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
+            child: movementsAsync.maybeWhen(
+              data: (movements) {
+                return ListBody(
+                  children: movements
+                      .map(
+                        (movement) =>
+                            WorkoutCardMovementLine(movement: movement),
+                      )
+                      .toList(),
+                );
+              },
+              orElse: () => const SizedBox(),
+            ),
+          ),
+        ],
       ),
     );
   }
