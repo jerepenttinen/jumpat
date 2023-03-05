0c1ac0ce802dbc8de9e99416fe6894aa7609c979
Jere Penttinen <jere@disroot.org>
2023-03-05 16:40:15 +0200

    Add export database

--- lib/features/workout/presentation/pages/backup_page.dart
+++ lib/features/workout/presentation/pages/backup_page.dart
@@ -8,6 +8,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:jumpat/features/core/providers.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
 
 class BackupPage extends ConsumerWidget {
   const BackupPage({super.key});
@@ -18,43 +19,60 @@ class BackupPage extends ConsumerWidget {
     return Scaffold(
       appBar: AppBar(title: Text(t.backupTitle)),
       body: Center(
-        child: TextButton(
-          child: Text(t.importDatabase),
-          onPressed: () async {
-            final result = await FilePicker.platform.pickFiles();
-            if (result == null) {
-              return;
-            }
+        child: Column(
+          children: [
+            TextButton(
+              child: Text(t.importDatabase),
+              onPressed: () async {
+                final result = await FilePicker.platform.pickFiles();
+                if (result == null) {
+                  return;
+                }
 
-            try {
-              final newDb = File(result.files.single.path!);
-              final headerString =
-                  await newDb.openRead(0, 16).transform(utf8.decoder).first;
-              if (!headerString.contains('SQLite format 3')) {
-                // :-D
-                throw Exception();
-              }
+                try {
+                  final newDb = File(result.files.single.path!);
+                  final headerString =
+                      await newDb.openRead(0, 16).transform(utf8.decoder).first;
+                  if (!headerString.contains('SQLite format 3')) {
+                    // :-D
+                    throw Exception();
+                  }
 
-              final dbFolder = await getApplicationDocumentsDirectory();
+                  final dbFolder = await getApplicationDocumentsDirectory();
 
-              await newDb.copy(p.join(dbFolder.path, 'jumpat.sqlite'));
+                  await newDb.copy(p.join(dbFolder.path, 'jumpat.sqlite'));
 
-              if (context.mounted) {
-                ref.invalidate(appDatabaseProvider);
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text(t.databaseImported)),
+                  if (context.mounted) {
+                    ref.invalidate(appDatabaseProvider);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text(t.databaseImported)),
+                    );
+                    Navigator.of(context).pop();
+                  }
+                } on Exception {
+                  if (context.mounted) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text(t.invalidFileFormat)),
+                    );
+                  }
+                  return;
+                }
+              },
+            ),
+            TextButton(
+              onPressed: () async {
+                final dbFolder = await getApplicationDocumentsDirectory();
+                final dbPath = p.join(dbFolder.path, 'jumpat.sqlite');
+                await Share.shareXFiles(
+                  [
+                    XFile(dbPath),
+                  ],
+                  text: 'database',
                 );
-                Navigator.of(context).pop();
-              }
-            } on Exception {
-              if (context.mounted) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text(t.invalidFileFormat)),
-                );
-              }
-              return;
-            }
-          },
+              },
+              child: Text(t.exportDatabase),
+            )
+          ],
         ),
       ),
     );
